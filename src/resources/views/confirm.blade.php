95591f456f1d8bf0b40f27d7e622f23cc47715f0
yuriko <yuriko40223@gmail.com>
2025-12-29 19:35:27 +0900

    レイアウト（確認ページ、サンクスページ）

--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -1,91 +1,74 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>確認ページ</title>
-    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
-    <link rel="stylesheet" href="{{asset('css/confirm.css')}}">
-    
-</head>
-<body>
-    <header id="header">
-        <h1 class="site-title">FashionablyLate</h1>
-    </header>
-    <main>
-        <h2 class="page-title">Confirm</h2>
+@extends('layouts.app')
 
-        <div class="wrapper">
+@section('css')
+    <link rel="stylesheet" href="{{asset('css/confirm.css')}}">
+@endsection
+
+@section('content')
+    <div class="wrapper">
+        <h2 class="page-title">Confirm</h2>
             <form action="/contacts" class="" method="post">
                 @csrf
-                <table>
-                    <tr>
-                        <th>お名前</th>
-                        <td>{{ $contacts['first_name']}}
-                            &nbsp;{{ $contacts['last_name']}}
-                        </td>
-                       <input type="hidden" name="first_name" value="{{$contacts['first_name']}}"> 
-                        <input type="hidden" name="last_name" value="{{$contacts['last_name']}}">
-                    </tr>
-                    <tr>
-                        <th>性別</th>
-                        <td>
-                            @if($contacts['gender']==1)
-                            男性
-                            @elseif($contacts['gender']==2)
-                            女性
-                            @else 
+            <table>
+                <tr>
+                    <th>お名前</th>
+                    <td>{{ $contacts['first_name']}}&nbsp;{{ $contacts['last_name']}}
+                    </td>
+                    <input type="hidden" name="first_name" value="{{$contacts['first_name']}}"> 
+                    <input type="hidden" name="last_name" value="{{$contacts['last_name']}}">
+                </tr>
+                <tr>
+                    <th>性別</th>
+                    <td>
+                        @if($contacts['gender']==1)男性
+                        @elseif($contacts['gender']==2)女性
+                        @else 
                             その他
-                            @endif
-                        </td>
+                        @endif
+                    </td>
                         <!-- ラジオボタンからinputにするには どうするか？-->
-                         <input type="hidden" name="gender" value="{{ $contacts['gender']}}"> 
-                    </tr>
-                    <tr>
-                        <th>メールアドレス</th>
-                        <td>{{ $contacts['email']}}"</td>
-                         <input type="hidden" name="email" value="{{$contacts['email']}}"> 
-                    </tr>
-                    <tr>
-                        <th>電話番号</th>
-                        <td>{{ $contacts['tel_1']}}{{ $contacts['tel_2']}}{{ $contacts['tel_3']}}</td>
-                        <input type="hidden" name="tel_1" value="{{$contacts['tel_1']}}"> 
-                        <input type="hidden" name="tel_2" value="{{$contacts['tel_2']}}">
-                        <input type="hidden" name="tel_3" value="{{$contacts['tel_3']}}">  
+                    <input type="hidden" name="gender" value="{{ $contacts['gender']}}"> 
+                </tr>
+                <tr>
+                    <th>メールアドレス</th>
+                    <td>{{ $contacts['email']}}</td>
+                    <input type="hidden" name="email" value="{{$contacts['email']}}"> 
+                </tr>
+                <tr>
+                    <th>電話番号</th>
+                    <td>{{ $contacts['tel_1']}}{{ $contacts['tel_2']}}{{ $contacts['tel_3']}}</td>
+                    <input type="hidden" name="tel_1" value="{{$contacts['tel_1']}}"> 
+                    <input type="hidden" name="tel_2" value="{{$contacts['tel_2']}}">
+                    <input type="hidden" name="tel_3" value="{{$contacts['tel_3']}}">  
             
-                    </tr>
-                    <tr>
-                        <th>住所</th>
-                        <td>{{ $contacts['address']}}</td>
-                        <input type="hidden" name="address" value="{{$contacts['address']}}"> 
-                    </tr>
-                    <tr>
-                        <th>建物名</th>
-                        <td>{{ $contacts['building']}}</td>
-                        <input type="hidden" name="building" value="{{$contacts['building']}}"> 
-                    </tr>
-                    <tr>
-                        <th>お問い合わせの種類</th>
-                        <td>{{ $category->content}}</td>
-                       
-                         <input type="hidden" name="category_id" value="{{ $contacts['category_id'] }}">
-
-                    </tr>
-                    <tr>
-                        <th>お問い合わせ内容</th>
-                        <td>{{$contacts['detail']}}
-                        </td>
-                         <input type="hidden" name="detail" value="{{$contacts['detail']}}"> 
-                    </tr>
-
-                </table>
-                <div class="btns">
-                    <button class="to-send">送信</button>
+                </tr>
+                <tr>
+                    <th>住所</th>
+                    <td>{{ $contacts['address']}}</td>
+                    <input type="hidden" name="address" value="{{$contacts['address']}}"> 
+                </tr>
+                <tr>
+                    <th>建物名</th>
+                    <td>{{ $contacts['building']}}</td>
+                    <input type="hidden" name="building" value="{{$contacts['building']}}"> 
+                </tr>
+                <tr>
+                    <th>お問い合わせの種類</th>
+                    <td>{{ $category->content}}</td>
+                    <input type="hidden" name="category_id" value="{{ $contacts['category_id'] }}">
+                </tr>
+                <tr>
+                    <th>お問い合わせ内容</th>
+                    <td>{{$contacts['detail']}}
+                    </td>
+                    <input type="hidden" name="detail" value="{{$contacts['detail']}}"> 
+                </tr>
+            </table>
+            <div class="btns">
+                <button class="to-send">送信</button>
                     <!-- <a href="/" class="edit-btn">修正</a> -->
-                    <input type="submit" class="update" name="back" value="修正">
-                </div>
+                <input type="submit" class="update" name="back" value="修正">
+            </div>
             </form>
-        </div>
-    </main>    
-</body>
-</html>
+    </div>
+@endsection    
--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -1,24 +1,13 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>サンクスページ</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inika:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+@extends('layouts.app')
+
+@section('css')
     <link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
-</head>
-<body>
-    
+@endsection
 
-        <div class="wrapper">
-            <h1 class="message">Thank you</h1>
-            <p>お問い合わせありがとうございました</p>
-            <a href="/" class="to-home">HOME</a>
-             <!-- <button class="to-home">HOME</button> -->
-
-        </div>
-</body>
-</html>  
+@section('content')
+    <div class="wrapper">
+        <h1 class="message">Thank you</h1>
+        <p>お問い合わせありがとうございました</p>
+        <a href="/" class="to-home">HOME</a>
+    </div>
+@endsection 
